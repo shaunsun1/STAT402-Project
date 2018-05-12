e7d39a583d54272c090dd33013250dafd969f648
Jonathan Fitz <Jonathan.Fitz@student.ufv.ca>
2018-05-11 19:06:07 -0700

    Added our work from using the survey package

--- Case_Study_1.R
+++ Case_Study_1.R
@@ -1,12 +1,12 @@
 # Load some useful packages
-library(tidyverse)  # a set of packages including ggplot2
-library(caret)  # used for knn and cross-validation
-library(car)  # calculates VIF to check for multicollinearity
+library(tidyverse)  # Set of packages including ggplot2
+library(caret)  # Used for knn and cross-validation
+library(car)  # Calculates VIF to check for multicollinearity
 library(ResourceSelection)  # Hosmer-Lemeshow goodness-of-fit test
+library(survey)  # Build glm using weights
 
-# Bring the chms_2018 dataset into R, and remove the weights as they are not used in this analysis
+# Bring the chms_2018 dataset into R
 study_data = read.csv("chms_2018.csv")
-study_data = select(study_data, -starts_with("BS"), -starts_with("WGT"))
 
 # Make SMK_12 a factor
 study_data$SMK_12 = as.factor(study_data$SMK_12)
@@ -16,9 +16,12 @@ study_data$SMK_12 = as.factor(study_data$SMK_12)
 seed1 = 336741157
 seed2 = 874515716
 
+
 #----------------------------------------------------------------------------------------------------------------
-# The values for LAB_BCD and LAB_BHG have been set to 999.5 when the patients recorded values are too low to 
-# be measured (ie. they are below the LOD). First understand how common this is
+# Estimate censored data
+
+# The values for LAB_BCD and LAB_BHG have been set to 999.5 when the patients recorded values are too low to be measured (ie. they are below the LOD). 
+# First understand how common this is
 BCD_LOD = sum(study_data$LAB_BCD == 999.5, na.rm = TRUE)  
 BCD_LOD  # 999.5 appears 54 times in LAB_BCD
 BHG_LOD = sum(study_data$LAB_BHG == 999.5, na.rm = TRUE)
@@ -33,6 +36,22 @@ study_data$LAB_BHG[study_data$LAB_BHG == 999.5 & !is.na(study_data$LAB_BHG)] = r
 # Confirm that all values below the LOD have been replaced.
 sum(study_data$LAB_BCD == 999.5, na.rm = TRUE)  # 999.5 appears 0 times in LAB_BCD
 sum(study_data$LAB_BHG == 999.5, na.rm = TRUE)  # 999.5 appears 0 times in LAB_BHG
+
+
+# Alternative method: Use survival analysis to estimate missing values!
+# Need to estimate both the distribution of the response, and also come up with an appropriate link function
+
+# Assuming limit of detection is same for all people, this is just left-censored data
+# Plot non-missing and non-censored values of LAB_BHG
+#ggplot(study_data[study_data$LAB_BHG < 900, ], aes(LAB_BHG)) + geom_histogram(binwidth = 2)
+
+# Do the same for LAB_BCD
+#ggplot(study_data[study_data$LAB_BCD < 900, ], aes(LAB_BCD)) + geom_histogram(binwidth = 1)
+
+# Plot weibull distributions with different parameters
+#ggplot(mapping = aes(rweibull(3000, shape = 1, scale = 15))) + geom_histogram(binwidth = 2)
+
+# Not done yet...
 
 
 #----------------------------------------------------------------------------------------------------------------
@@ -60,24 +79,25 @@ ctrl <- trainControl(method="cv", number = 5)
 
 # Using HWMDBMI as the response variable, find the best value of K using as the training set only the observations 
 # that have no missing values
-knn_HWMDBMI <- train(HWMDBMI ~ . -CLINICID, data = study_data, subset = (multiple_missing == 0), method = "knn", preProcess = c("center","scale"), trControl = ctrl, tuneLength = 30)
+knn_HWMDBMI <- train(HWMDBMI ~ as.factor(HIGHBP)+SMK_12+CLC_SEX+CLC_AGE+LAB_BCD+LAB_BHG, data = study_data, subset = (multiple_missing == 0), method = "knn", preProcess = c("center","scale"), trControl = ctrl, tuneLength = 30)
 # Estimate the missing values in HWMDBMI
 study_data$HWMDBMI[is.na(study_data$HWMDBMI)] = predict(knn_HWMDBMI, study_data[is.na(study_data$HWMDBMI),])
 
 # Repeat the above steps for the other variables with missing data
-knn_LAB_BCD <- train(LAB_BCD ~ . -CLINICID, data = study_data, subset = (multiple_missing == 0), method = "knn", preProcess = c("center","scale"), trControl = ctrl, tuneLength = 30)
+knn_LAB_BCD <- train(LAB_BCD ~ HWMDBMI+as.factor(HIGHBP)+SMK_12+CLC_SEX+CLC_AGE+LAB_BHG, data = study_data, subset = (multiple_missing == 0), method = "knn", preProcess = c("center","scale"), trControl = ctrl, tuneLength = 30)
 study_data$LAB_BCD[is.na(study_data$LAB_BCD)] = predict(knn_LAB_BCD, study_data[is.na(study_data$LAB_BCD),])
 
-knn_LAB_BHG <- train(LAB_BHG ~ . -CLINICID, data = study_data, subset = (multiple_missing == 0), method = "knn", preProcess = c("center","scale"), trControl = ctrl, tuneLength = 30)
+knn_LAB_BHG <- train(LAB_BHG ~ LAB_BCD+HWMDBMI+as.factor(HIGHBP)+SMK_12+CLC_SEX+CLC_AGE, data = study_data, subset = (multiple_missing == 0), method = "knn", preProcess = c("center","scale"), trControl = ctrl, tuneLength = 30)
 study_data$LAB_BHG[is.na(study_data$LAB_BHG)] = predict(knn_LAB_BHG, study_data[is.na(study_data$LAB_BHG),])
 
-knn_SMK_12 <- train(SMK_12 ~ . -CLINICID, data = study_data, subset = (multiple_missing == 0), method = "knn", preProcess = c("center","scale"), trControl = ctrl, tuneLength = 30)
+knn_SMK_12 <- train(SMK_12 ~ LAB_BHG+LAB_BCD+HWMDBMI+as.factor(HIGHBP)+CLC_SEX+CLC_AGE, data = study_data, subset = (multiple_missing == 0), method = "knn", preProcess = c("center","scale"), trControl = ctrl, tuneLength = 30)
 study_data$SMK_12[is.na(study_data$SMK_12)] = predict(knn_SMK_12, study_data[is.na(study_data$SMK_12),])
 
 sum(is.na(study_data))  # 0 missing values
 
 
 #----------------------------------------------------------------------------------------------------------------
+# Do some EDA
 ggplot(study_data, aes(cut_number(CLC_AGE, 20), fill = factor(HIGHBP))) + 
   geom_bar(position = position_fill(reverse = TRUE)) +
   xlab("Age") + 
@@ -114,7 +134,9 @@ ggplot(study_data, aes(cut_number(LAB_BHG, 20), fill = factor(HIGHBP))) +
 #----------------------------------------------------------------------------------------------------------------
 # Make CLC_AGE into a categorical variable with three levels, and add this to our data frame
 study_data = mutate(study_data, CLC_AGE_CAT = as.factor(cut_width(study_data$CLC_AGE, 20, boundary = 20, closed = "left")))
+study_data = study_data[, c(1:8, 510, 9:509)]  # Reorder study_data columns to make life easier
 
+# Build three models each using a different link function
 # Uses log odds link function
 model_logit = glm(as.factor(HIGHBP) ~ SMK_12+CLC_SEX+CLC_AGE_CAT+HWMDBMI+LAB_BCD+LAB_BHG, binomial(link = 'logit'), study_data)
 
@@ -126,6 +148,8 @@ model_cloglog = glm(as.factor(HIGHBP) ~ SMK_12+CLC_SEX+CLC_AGE_CAT+HWMDBMI+LAB_B
 
 
 #----------------------------------------------------------------------------------------------------------------
+# Hosmer-Lemeshow statistic
+
 # See that the minimum estimated probabilities that HIGHBP=2 are all greater than 10%
 min(fitted.values(model_logit))
 min(fitted.values(model_probit))
@@ -138,9 +162,7 @@ max(fitted.values(model_cloglog))
 
 # Calculate the Hosmer-Lemeshow statistic multiple times for each of the three models
 pvalues_logit = sapply(X = seq(10, 60, 1), FUN = hoslem.test, x = model_logit$y, y = fitted.values(model_logit))["p.value",]
-
 pvalues_probit = sapply(X = seq(10, 60, 1), FUN = hoslem.test, x = model_probit$y, y = fitted.values(model_probit))["p.value",]
-
 pvalues_cloglog = sapply(X = seq(10, 60, 1), FUN = hoslem.test, x = model_cloglog$y, y = fitted.values(model_cloglog))["p.value",]
 
 pvalues_logit = unlist(pvalues_logit)
@@ -155,7 +177,10 @@ ggplot(mapping = aes(x = factor(rep(c("Logit", "Probit", "Cloglog"),  each = 51)
   ggtitle("Different Link Functions have Similiar P-values for Hosmer-Lemeshow Statistic") +
   theme(plot.title = element_text(hjust = 0.5))
 
+
 #----------------------------------------------------------------------------------------------------------------
+# Continue model adequacy checking
+
 # Look at two models, one with CLC_AGE and one with CLC_AGE_CAT, and compare their AIC.
 summary(model_logit)
 summary(update(model_logit, ~ . -CLC_AGE_CAT+CLC_AGE))
@@ -170,13 +195,66 @@ ggplot(mapping = aes(model_logit$linear.predictors, residuals(model_logit, "pear
   ggtitle("Residual Plot with No Replicates is Uninformative") +
   theme(plot.title = element_text(hjust = 0.5))
 
+
 #----------------------------------------------------------------------------------------------------------------
+# Draw conclusions
 drop1(model_logit, test = "LRT")
+
+# Even when taking into account the other variables in the model, there is strong evidence that sex, age, and 
+# body mass index influence average hypertension, and weaker evidence that blood mercury levels (LAB_BHG) and 
+# smoking status have an effect. It is inconclusive whether blood cadmium levels (LAB_BCD) affect average hypertension
 
 # Test whether the interaction terms between CLC_SEX and other variables are non-zero
 model_gender = update(model_logit, ~ . + CLC_SEX:SMK_12 + CLC_SEX:CLC_AGE_CAT + CLC_SEX:HWMDBMI + CLC_SEX:LAB_BCD + CLC_SEX:LAB_BHG)
 drop1(model_gender, test = "LRT")
 
+# There is some weak evidence that the effect of body mass index on average hypertension does differ between genders, 
+# but little evidence that the effect of other variables differ by gender
+
 # Test whether the interaction terms between CLC_AGE_CAT and other variables are non-zero
 model_age = update(model_logit, ~ . + CLC_AGE_CAT:SMK_12 + CLC_AGE_CAT:CLC_SEX + CLC_AGE_CAT:HWMDBMI + CLC_AGE_CAT:LAB_BCD + CLC_AGE_CAT:LAB_BHG)
 drop1(model_age, test = "LRT")
+
+# There is little evidence here to suggest that the risk factors of hypertension differ on average between age groups
+
+
+#----------------------------------------------------------------------------------------------------------------
+# Try to use glm function to take into account survey weights
+model_logit_weights = glm(as.factor(HIGHBP) ~ SMK_12+CLC_SEX+CLC_AGE_CAT+HWMDBMI+LAB_BCD+LAB_BHG, family=binomial(), study_data, WGT_FULL)
+
+# This gives us warning messages however
+# The 'weights' variable in the glm function is the number of trials for each observation (according to glm() help page), 
+# which is not what we want (at the very least because our weights are not integers)
+
+# Instead, use the package 'survey'
+# Here, 'weights' represent the sample weights, and 'repweights' are the replicate weights 
+glm_design = svrepdesign(study_data[, c(2, 3, 5:9)], repweights = select(study_data, starts_with("BS")), weights = study_data$WGT_FULL, type = 'bootstrap')
+model_with_weights = svyglm(as.factor(HIGHBP) ~ SMK_12+CLC_SEX+CLC_AGE_CAT+HWMDBMI+LAB_BCD+LAB_BHG, design = glm_design, family = quasibinomial(), data = study_data)
+
+# There are many questions that need to be answered though about these functions:
+
+# 1) In svrepdesign function, are the replication weights (our bootstrap weights) really of type 'bootstrap'? (see help page)
+#    Need to better understand what Canadian Health Measures Survey means by 'bootstrap weights', and what author of survey package means by 'boostrap' replication weights
+
+# 2) In svrepdesign function, not specifying repweights produces the warning: 'You must provide replication weights' 
+#    Why is this?
+
+# 3) In svyglm function, letting family = binomial() produces the warning: 'In eval(family$initialize) : non-integer #successes in a binomial glm!'
+#    Both the svyglm help file and the survey author's textbook (p.110) recommend using family = quasibinomial() to avoid these warnings, but no clear rationale is given
+
+# 4) In svrepdesign function, what do we provide for combined.weights, scale, fpc, etc?
+
+# 5) According to survey author's textbook (p.98), svyglm() does not seem to use maximum likelihood to estimate parameters,
+#    and author concludes that we can't use likelihood ratio tests to compare nested models, but should use Wald statistic instead
+#    So many questions here
+
+# Hopefully, working through some of the author's analyzed datasets will provide some of these answers
+
+# Compute Wald statistics 
+summary(model_with_weights)
+
+# According to the Wald Statistics, Age, HWMDBMI, Sex, and LAB_BHG have the lowest p-values. Everything else has high p-values
+# This gives similiar p-values to when weights were not used
+
+# Shows references that the survey package relies on
+citation('survey')
